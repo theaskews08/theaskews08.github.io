46f821e0d9caf363aaea4dfcc2b79e3109ab4edd
B <theaskews08@gmail.com>
2023-08-26 17:35:06 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Simulate reading from a CSV file (replace this with actual CSV reading)
+  const df = [
+    { "PLU Code": "4011", "Name": "Banana" },
+    { "PLU Code": "4012", "Name": "Apple" },
+    // Add more items here
+  ];
 
+  const searchInput = document.getElementById("searchInput");
+  const outputLabel = document.getElementById("outputLabel");
+  const clearButton = document.getElementById("clearButton");
+  const exitButton = document.getElementById("exitButton");
+
+  searchInput.addEventListener("input", function () {
+    const searchFor = searchInput.value.trim();
+    if (searchFor) {
+      const results = df.filter(
+        (row) =>
+          row["Name"].toLowerCase().includes(searchFor.toLowerCase()) ||
+          row["PLU Code"].includes(searchFor)
+      );
+      const resultsStr = results
+        .map((row) => `${row["PLU Code"]} - ${row["Name"]}`)
+        .join("\n");
+      outputLabel.textContent = resultsStr;
+    } else {
+      outputLabel.textContent = "";
+    }
+  });
+
+  clearButton.addEventListener("click", function () {
+    searchInput.value = "";
+    outputLabel.textContent = "";
+  });
+
+  exitButton.addEventListener("click", function () {
+    window.close();
+  });
+});
