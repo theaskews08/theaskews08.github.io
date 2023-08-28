eb378e8ade80fa9a952b6f9c5a6a7d57a86704e0
B <theaskews08@gmail.com>
2023-08-28 07:25:01 -0600

    Update fetchPLU.js

--- fetchPLU.js
+++ fetchPLU.js
@@ -1,1 +1,6 @@
-
+fetch('path/to/your/plu_data.json')
+  .then(response => response.json())
+  .then(data => {
+    pluData = data; // Make sure 'pluData' is declared in the global scope or accessible here.
+  })
+  .catch(error => console.error('Error fetching PLU data:', error));
