ec8a853e9170b02a741dd61a11a5feb2261bd0b5
B <theaskews08@gmail.com>
2023-08-28 12:36:02 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,42 @@
-object; it should be filled by your fetchPLU.js
-let pluData = {};
+let pluData = [];
 
-// Fetch PLU data from JSON file
-fetch('./plu_data.json')
-  .then(response => response.json())
-  .then(data => {
-    pluData = data;
-  })
-  .catch(error => console.error('Error fetching PLU data:', error));
-
-// Get DOM elements
-const searchInput = document.getElementById('searchInput');
-const outputLabel = document.getElementById('outputLabel');
-const clearButton = document.getElementById('clearButton');
-const exitButton = document.getElementById('exitButton');
-
-// Event Listeners
-searchInput.addEventListener('input', function() {
-  const searchValue = searchInput.value.toLowerCase();
-  const plu = pluData.find(item => item.Name.toLowerCase().includes(searchValue));
-  
-  if (plu) {
-    outputLabel.textContent = `Found: ${plu.Name}, PLU Code: ${plu['PLU Code']}`;
-  } else {
-    outputLabel.textContent = 'Not Found';
+async function fetchData() {
+  try {
+    const response = await fetch('./plu_data.json');
+    pluData = await response.json();
+    initializeApp();
+  } catch (error) {
+    console.error('Error fetching PLU data:', error);
   }
-});
+}
 
-clearButton.addEventListener('click', function() {
-  searchInput.value = '';
-  outputLabel.textContent = '';
-});
+function initializeApp() {
+  // Get DOM elements
+  const searchInput = document.getElementById('searchInput');
+  const outputLabel = document.getElementById('outputLabel');
+  const clearButton = document.getElementById('clearButton');
+  const exitButton = document.getElementById('exitButton');
 
-exitButton.addEventListener('click', function() {
-  window.close();
-});
+  // Event Listeners
+  searchInput.addEventListener('input', function() {
+    const searchValue = searchInput.value.toLowerCase();
+    const plu = pluData.find(item => item.Name.toLowerCase().includes(searchValue));
+    
+    if (plu) {
+      outputLabel.textContent = `Found: ${plu.Name}, PLU Code: ${plu['PLU Code']}`;
+    } else {
+      outputLabel.textContent = 'Not Found';
+    }
+  });
+
+  clearButton.addEventListener('click', function() {
+    searchInput.value = '';
+    outputLabel.textContent = '';
+  });
+
+  exitButton.addEventListener('click', function() {
+    window.close();
+  });
+}
+
+fetchData();
