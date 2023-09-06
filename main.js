aa14859f07bfd31c1ce125b13ab2a21ff83c3e8c
B <theaskews08@gmail.com>
2023-09-06 08:10:02 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,70 +7,19 @@ const searchInput = document.getElementById('searchInput');
 const outputLabel = document.getElementById('outputLabel');
 const clearButton = document.getElementById('clearButton');
 const exitButton = document.getElementById('exitButton');
-const saveButton = document.getElementById('saveButton');
 const exportButton = document.getElementById('exportButton');
-const savedSearchesDiv = document.getElementById('savedSearches');
 
 // Event Listeners
 searchInput.addEventListener('input', function() {
-  const searchTerm = searchInput.value.toLowerCase();
-  let foundItems = pluData.filter(item => 
-    item.Name.toLowerCase().includes(searchTerm) || 
-    item['PLU Code'].toString().includes(searchTerm)
-  );
-  
-  // Sort the items alphabetically
-  foundItems.sort((a, b) => a.Name.localeCompare(b.Name));
-  
-  // Clear the output label
-  outputLabel.innerHTML = '';
-
-  // Show the first 5 matched items as suggestions
-  for(let i = 0; i < Math.min(5, foundItems.length); i++) {
-    outputLabel.innerHTML += `<a href="https://www.google.com/search?q=${foundItems[i].Name}">${foundItems[i].Name}</a> (PLU Code: ${foundItems[i]['PLU Code']})<br>`;
-  }
-  
-  if (foundItems.length === 0) {
-    outputLabel.textContent = 'Not found';
-  }
-});
-
-searchInput.addEventListener('keydown', function(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    searchInput.blur();
-  }
+  // ... (same as before)
 });
 
 clearButton.addEventListener('click', function() {
-  searchInput.value = '';
-  outputLabel.textContent = '';
+  // ... (same as before)
 });
 
 exitButton.addEventListener('click', function() {
-  window.close();
-});
-
-saveButton.addEventListener('click', function() {
-  const currentSearch = searchInput.value;
-  if (currentSearch) {
-    const foundItems = pluData.filter(item => 
-      item.Name.toLowerCase().includes(currentSearch.toLowerCase()) || 
-      item['PLU Code'].toString().includes(currentSearch)
-    );
-    savedSearches.push(...foundItems);
-    updateSavedSearches();
-  }
-});
-
-exportButton.addEventListener('click', function() {
-  const blob = new Blob([savedSearches.map(item => `${item.Name}, ${item['PLU Code']}`).join('\n')], { type: 'text/plain' });
-  const url = URL.createObjectURL(blob);
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = 'saved_searches.txt';
-  a.click();
-  URL.revokeObjectURL(url);
+  // ... (same as before)
 });
 
 // Fetch data from plu_data.json and update pluData
@@ -80,10 +29,3 @@ fetch('./plu_data.json')
     pluData = data;
   })
   .catch(error => console.error('Error fetching PLU data:', error));
-
-function updateSavedSearches() {
-  savedSearchesDiv.innerHTML = '';
-  savedSearches.forEach(item => {
-    savedSearchesDiv.innerHTML += `<p>${item.Name} (PLU Code: ${item['PLU Code']})</p>`;
-  });
-}
