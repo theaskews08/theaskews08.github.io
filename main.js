1dec7add956a3320af0383cbad79890dbf63778c
B <theaskews08@gmail.com>
2023-09-07 07:46:41 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Initialize empty array for PLU data
   let pluData = [];
-
-  // Get DOM elements
   const searchInput = document.getElementById('searchInput');
   const outputLabel = document.getElementById('outputLabel');
   const clearButton = document.getElementById('clearButton');
   const exitButton = document.getElementById('exitButton');
 
-  // Event listeners and your existing logic for search, clear, and exit go here...
-
-  // Fetch data from plu_data.json and update pluData
-  fetch('./plu_data.json')  
+  fetch('./plu_data.json')
     .then(response => response.json())
     .then(data => {
       pluData = data;
     })
     .catch(error => console.error('Error fetching PLU data:', error));
 
-  // Move the screen up when search input is focused
-  searchInput.addEventListener('focus', function() {
-    document.body.style.position = 'fixed';
-    document.body.style.bottom = '50px';
+  searchInput.addEventListener('input', function() {
+    // Your search logic here
   });
 
-  // Reset the screen position when search input loses focus
-  searchInput.addEventListener('blur', function() {
-    document.body.style.position = '';
-    document.body.style.bottom = '';
-  });
+  // Your other event listeners and logic...
 });
