fa848f60e8e3463502c801c523b432bbd1ab43ff
B <theaskews08@gmail.com>
2023-09-07 06:59:59 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-// Initialize empty array for PLU data
-let pluData = [];
+document.addEventListener('DOMContentLoaded', function() {
+  // Initialize empty array for PLU data
+  let pluData = [];
 
-// Get DOM elements
-const searchInput = document.getElementById('searchInput');  // Matched with HTML id
-const outputLabel = document.getElementById('outputLabel');
-const clearButton = document.getElementById('clearButton');
-const exitButton = document.getElementById('exitButton');
+  // Get DOM elements
+  const searchInput = document.getElementById('searchInput');
+  const outputLabel = document.getElementById('outputLabel');
+  const clearButton = document.getElementById('clearButton');
+  const exitButton = document.getElementById('exitButton');
 
-// Your existing event listeners and logic here...
+  // Event listeners and your existing logic for search, clear, and exit go here...
 
-// Fetch data from plu_data.json and update pluData
-fetch('./plu_data.json')  
-  .then(response => response.json())
-  .then(data => {
-    pluData = data;
-  })
-  .catch(error => console.error('Error fetching PLU data:', error));
+  // Fetch data from plu_data.json and update pluData
+  fetch('./plu_data.json')  
+    .then(response => response.json())
+    .then(data => {
+      pluData = data;
+    })
+    .catch(error => console.error('Error fetching PLU data:', error));
 
-window.addEventListener('DOMContentLoaded', function() {
+  // Move the screen up when search input is focused
   searchInput.addEventListener('focus', function() {
     document.body.style.position = 'fixed';
     document.body.style.bottom = '50px';
   });
 
+  // Reset the screen position when search input loses focus
   searchInput.addEventListener('blur', function() {
     document.body.style.position = '';
     document.body.style.bottom = '';
