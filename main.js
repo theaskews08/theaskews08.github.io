de3d5857e52fbea776ae1edb0b700e5297c6673e
B <theaskews08@gmail.com>
2023-09-09 16:18:09 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,34 +6,28 @@ const searchInput = document.getElementById('searchInput');
 const outputLabel = document.getElementById('outputLabel');
 const clearButton = document.getElementById('clearButton');
 const exitButton = document.getElementById('exitButton');
-const voiceButton = document.getElementById('voiceButton');
 
 // Event Listeners
 searchInput.addEventListener('input', function() {
   const searchTerm = searchInput.value.toLowerCase().split(' ');
-
   let foundItems = pluData.filter(item => {
     const itemNameTokens = item.Name.toLowerCase().split(' ');
 
     return searchTerm.every(term => itemNameTokens.some(token => token.includes(term))) ||  
-    item['PLU Code'].toString().includes(searchTerm.join(' '));
+           item['PLU Code'].toString().includes(searchTerm.join(' '));
   });
 
-  foundItems.sort((a, b) => a.Name.localeCompare(b.Name));
+  // Clear the output label
   outputLabel.innerHTML = '';
 
+  // Sort and show the first 5 matched items as suggestions
+  foundItems.sort((a, b) => a.Name.localeCompare(b.Name));
   for(let i = 0; i < Math.min(5, foundItems.length); i++) {
     outputLabel.innerHTML += `<a href="https://www.google.com/search?q=${foundItems[i].Name}">${foundItems[i].Name}</a> (PLU Code: ${foundItems[i]['PLU Code']})<br>`;
   }
 
   if (foundItems.length === 0) {
     outputLabel.textContent = 'Not found';
-  }
-});
-
-searchInput.addEventListener('keyup', function(event) {
-  if (event.keyCode === 13) {
-    searchInput.blur();
   }
 });
 
@@ -44,10 +38,6 @@ clearButton.addEventListener('click', function() {
 
 exitButton.addEventListener('click', function() {
   window.close();
-});
-
-voiceButton.addEventListener('click', function() {
-  // Implement voice search here
 });
 
 // Fetch data from plu_data.json and update pluData
