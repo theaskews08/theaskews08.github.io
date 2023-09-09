767f5e971ad38dc93fdcf3e42d705bf1c4922956
B <theaskews08@gmail.com>
2023-09-08 23:38:18 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,41 +1,49 @@
-// Initialize empty array for PLU data
 let pluData = [];
-
-// Get DOM elements
 const searchInput = document.getElementById('searchInput');
 const outputLabel = document.getElementById('outputLabel');
 const clearButton = document.getElementById('clearButton');
 const exitButton = document.getElementById('exitButton');
-const voiceButton = document.getElementById('voiceButton');  // New Voice Button
+const voiceSearch = document.getElementById('voiceSearch');
 
-// Event Listeners
-searchInput.addEventListener('input', function() {
-  // ...existing search code...
-});
-
-clearButton.addEventListener('click', function() {
-  // ...existing clear button code...
-});
-
-exitButton.addEventListener('click', function() {
-  // ...existing exit button code...
-});
-
-// New voice search code
-voiceButton.addEventListener('click', function() {
-  const recognition = new window.webkitSpeechRecognition();
-  recognition.lang = 'en-US';
+// Voice search functionality
+voiceSearch.addEventListener('click', function() {
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
 
   recognition.onresult = function(event) {
     const transcript = event.results[0][0].transcript;
     searchInput.value = transcript;
-    searchInput.dispatchEvent(new Event('input'));
+    search();
   };
 
   recognition.start();
 });
 
-// Fetch data
+searchInput.addEventListener('input', search);
+
+function search() {
+  const searchTerm = searchInput.value.toLowerCase();
+  let foundItems = pluData.filter(item => item.Name.toLowerCase().includes(searchTerm) || item['PLU Code'].toString().includes(searchTerm));
+  foundItems.sort((a, b) => a.Name.localeCompare(b.Name));
+  outputLabel.innerHTML = '';
+  
+  for(let i = 0; i < Math.min(5, foundItems.length); i++) {
+    outputLabel.innerHTML += `<a href="https://www.google.com/search?q=${foundItems[i].Name}">${foundItems[i].Name}</a> (PLU Code: ${foundItems[i]['PLU Code']})<br>`;
+  }
+  if (foundItems.length === 0) {
+    outputLabel.textContent = 'Not found';
+  }
+}
+
+clearButton.addEventListener('click', function() {
+  searchInput.value = '';
+  outputLabel.textContent = '';
+});
+
+exitButton.addEventListener('click', function() {
+  window.close();
+});
+
 fetch('./plu_data.json')
   .then(response => response.json())
   .then(data => {
