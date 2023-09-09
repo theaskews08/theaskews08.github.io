8cff32ff4e900fbb34b451f1dceb68cdd8d62fdb
B <theaskews08@gmail.com>
2023-09-09 09:51:09 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,21 +2,20 @@
 let pluData = [];
 
 // Get DOM elements
-const searchInput = document.querySelector('input[type="search"]');
+const searchInput = document.getElementById('searchInput');
 const outputLabel = document.getElementById('outputLabel');
 const clearButton = document.getElementById('clearButton');
 const exitButton = document.getElementById('exitButton');
 const voiceButton = document.getElementById('voiceButton');
 
-// Initialize SpeechRecognition API
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
+console.log("DOM elements fetched:", searchInput, outputLabel, clearButton, exitButton, voiceButton);
 
 // Event Listeners
-searchInput.addEventListener('input', function () {
+searchInput.addEventListener('input', function() {
+  console.log("Search input event triggered.");
   const searchTerm = searchInput.value.toLowerCase();
-  let foundItems = pluData.filter(item =>
-    item.Name.toLowerCase().includes(searchTerm) ||
+  let foundItems = pluData.filter(item =>  
+    item.Name.toLowerCase().includes(searchTerm) ||  
     item['PLU Code'].toString().includes(searchTerm)
   );
 
@@ -27,7 +26,7 @@ searchInput.addEventListener('input', function () {
   outputLabel.innerHTML = '';
 
   // Show the first 5 matched items as suggestions
-  for (let i = 0; i < Math.min(5, foundItems.length); i++) {
+  for(let i = 0; i < Math.min(5, foundItems.length); i++) {
     outputLabel.innerHTML += `<a href="https://www.google.com/search?q=${foundItems[i].Name}">${foundItems[i].Name}</a> (PLU Code: ${foundItems[i]['PLU Code']})<br>`;
   }
 
@@ -36,37 +35,37 @@ searchInput.addEventListener('input', function () {
   }
 });
 
-// Close keyboard on "Enter" key press
-searchInput.addEventListener('keyup', function (event) {
+searchInput.addEventListener('keyup', function(event) {
+  console.log("Keyup event triggered.");
   if (event.keyCode === 13) {
     searchInput.blur();
   }
 });
 
-clearButton.addEventListener('click', function () {
+clearButton.addEventListener('click', function() {
+  console.log("Clear button clicked.");
   searchInput.value = '';
   outputLabel.textContent = '';
 });
 
-exitButton.addEventListener('click', function () {
+exitButton.addEventListener('click', function() {
+  console.log("Exit button clicked.");
   window.close();
 });
 
-// Voice search functionality
-voiceButton.addEventListener('click', function () {
-  recognition.start();
-});
-
-recognition.addEventListener('result', function (event) {
-  const transcript = event.results[0][0].transcript;
-  searchInput.value = transcript;
-  searchInput.dispatchEvent(new Event('input'));
+voiceButton.addEventListener('click', function() {
+  console.log("Voice button clicked.");
+  // Implement voice search here
 });
 
 // Fetch data from plu_data.json and update pluData
-fetch('./plu_data.json')
-  .then(response => response.json())
+fetch('./plu_data.json')  
+  .then(response => {
+    console.log("Fetching PLU data...");
+    return response.json();
+  })
   .then(data => {
+    console.log("PLU data fetched successfully:", data);
     pluData = data;
   })
   .catch(error => console.error('Error fetching PLU data:', error));
