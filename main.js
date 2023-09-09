48a98bb00eaa2f49bdb0d2f5b9101aa90c779255
B <theaskews08@gmail.com>
2023-09-08 20:20:00 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,61 +1,56 @@
-// Initialize empty array for PLU data
-let pluData = [];
+// Initialize empty array for PLU data 
+let pluData = []; 
 
-// Get DOM elements
-const searchInput = document.getElementById('searchInput');
-const outputLabel = document.getElementById('outputLabel');
-const clearButton = document.getElementById('clearButton');
-const exitButton = document.getElementById('exitButton');
-const voiceButton = document.getElementById('voiceButton');
+// Get DOM elements 
+const searchInput = document.getElementById('searchInput'); 
+const outputLabel = document.getElementById('outputLabel'); 
+const clearButton = document.getElementById('clearButton'); 
+const exitButton = document.getElementById('exitButton'); 
 
-// Event Listeners
-searchInput.addEventListener('input', function() {
-  const searchTerm = searchInput.value.toLowerCase();
-  let foundItems = pluData.filter(item =>  
-    item.Name.toLowerCase().includes(searchTerm) ||  
-    item['PLU Code'].toString().includes(searchTerm)
-  );
+// Event Listeners 
+searchInput.addEventListener('input', function() { 
+     const searchTerm = searchInput.value.toLowerCase(); 
+     let foundItems = pluData.filter(item =>  
+                                   item.Name.toLowerCase().includes(searchTerm) ||  
+                                   item['PLU Code'].toString().includes(searchTerm) 
+                                  ); 
 
-  // Sort the items alphabetically
-  foundItems.sort((a, b) => a.Name.localeCompare(b.Name));
+     // Sort the items alphabetically 
+     foundItems.sort((a, b) => a.Name.localeCompare(b.Name)); 
 
-  // Clear the output label
-  outputLabel.innerHTML = '';
+     // Clear the output label 
+     outputLabel.innerHTML = ''; 
 
-  // Show the first 5 matched items as suggestions
-  for(let i = 0; i < Math.min(5, foundItems.length); i++) {
-    outputLabel.innerHTML += `<a href="https://www.google.com/search?q=${foundItems[i].Name}">${foundItems[i].Name}</a> (PLU Code: ${foundItems[i]['PLU Code']})<br>`;
-  }
+     // Show the first 5 matched items as suggestions 
+     for(let i = 0; i < Math.min(5, foundItems.length); i++) { 
+       outputLabel.innerHTML += `<a href="https://www.google.com/search?q=${foundItems[i].Name}">${foundItems[i].Name}</a> (PLU Code: ${foundItems[i]['PLU Code']})<br>`; 
+   } 
 
-  if (foundItems.length === 0) {
-    outputLabel.textContent = 'Not found';
-  }
-});
+     if (foundItems.length === 0) { 
+       outputLabel.textContent = 'Not found'; 
+   } 
+ }); 
 
-// Close keyboard on "Enter" key press
-searchInput.addEventListener('keyup', function(event) {
-  if (event.keyCode === 13) {
-    searchInput.blur();
-  }
-});
+// Close keyboard on "Enter" key press 
+searchInput.addEventListener('keyup', function(event) { 
+     if (event.keyCode === 13) { 
+       searchInput.blur(); 
+   } 
+ }); 
 
-clearButton.addEventListener('click', function() {
-  searchInput.value = '';
-  outputLabel.textContent = '';
-});
+clearButton.addEventListener('click', function() { 
+     searchInput.value = ''; 
+     outputLabel.textContent = ''; 
+ }); 
 
-exitButton.addEventListener('click', function() {
-  window.close();
-});
+exitButton.addEventListener('click', function() { 
+     window.close(); 
+ }); 
 
-voiceButton.addEventListener('click', function() {
-  // Implement voice search here
-});
-
-// Fetch data from plu_data.json and update pluData
-fetch('./plu_data.json')  
-  .then(response => response.json())
-  .then(data => {
-    pluData = data;
-  })
-  .catch(error => console.error('Error fetching PLU data:', error));
+// Fetch data from plu_data.json and update pluData 
+fetch('./plu_data.json')   
+     .then(response => response.json()) 
+     .then(data => { 
+     pluData = data; 
+ }) 
+     .catch(error => console.error('Error fetching PLU data:', error));
