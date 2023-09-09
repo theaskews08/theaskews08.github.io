36812acaf1a681b8fccffef033cdc8f38f2e6db9
B <theaskews08@gmail.com>
2023-09-09 00:02:14 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,59 +1,43 @@
 // Initialize empty array for PLU data
 let pluData = [];
 
+// Initialize speech recognition
+const recognition = new webkitSpeechRecognition();
+recognition.continuous = false;
+
 // Get DOM elements
 const searchInput = document.getElementById('searchInput');
-const micButton = document.getElementById('micButton');
 const outputLabel = document.getElementById('outputLabel');
-const clearButton = document.getElementById('clearButton');
-const exitButton = document.getElementById('exitButton');
+const voiceButton = document.getElementById('voiceButton'); // New element
 
 // Event Listeners
-searchInput.addEventListener('input', function () {
+searchInput.addEventListener('input', searchItems);
+
+// Add voice recognition event listener
+voiceButton.addEventListener('click', () => {
+  recognition.start();
+});
+
+recognition.addEventListener('result', function(event) {
+  searchInput.value = event.results[0][0].transcript;
+  searchItems();
+});
+
+function searchItems() {
   const searchTerm = searchInput.value.toLowerCase();
   let foundItems = pluData.filter(item =>
     item.Name.toLowerCase().includes(searchTerm) ||
     item['PLU Code'].toString().includes(searchTerm)
   );
 
-  // Sort the items alphabetically
   foundItems.sort((a, b) => a.Name.localeCompare(b.Name));
-
-  // Clear the output label
   outputLabel.innerHTML = '';
 
-  // Show the first 5 matched items as suggestions
   for (let i = 0; i < Math.min(5, foundItems.length); i++) {
     outputLabel.innerHTML += `<a href="https://www.google.com/search?q=${foundItems[i].Name}">${foundItems[i].Name}</a> (PLU Code: ${foundItems[i]['PLU Code']})<br>`;
   }
+}
 
-  if (foundItems.length === 0) {
-    outputLabel.textContent = 'Not found';
-  }
-});
-
-// Stub for microphone button functionality
-micButton.addEventListener('click', function () {
-  // Voice search code would go here
-});
-
-// Close keyboard on "Enter" key press
-searchInput.addEventListener('keyup', function (event) {
-  if (event.keyCode === 13) {
-    searchInput.blur();
-  }
-});
-
-clearButton.addEventListener('click', function () {
-  searchInput.value = '';
-  outputLabel.textContent = '';
-});
-
-exitButton.addEventListener('click', function () {
-  window.close();
-});
-
-// Fetch data from plu_data.json and update pluData
 fetch('./plu_data.json')
   .then(response => response.json())
   .then(data => {
