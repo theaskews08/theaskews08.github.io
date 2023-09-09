57490ddb4f2d7e5bb31c49fe3c44de3aa97cd31a
B <theaskews08@gmail.com>
2023-09-09 07:13:16 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,72 @@
-// Initialize empty array for PLU data
 let pluData = [];
 
-// Initialize speech recognition
-const recognition = new webkitSpeechRecognition();
-recognition.continuous = false;
-
-// Get DOM elements
 const searchInput = document.getElementById('searchInput');
 const outputLabel = document.getElementById('outputLabel');
-const voiceButton = document.getElementById('voiceButton'); // New element
+const clearButton = document.getElementById('clearButton');
+const exitButton = document.getElementById('exitButton');
+const voiceButton = document.getElementById('voiceButton');
 
-// Event Listeners
-searchInput.addEventListener('input', searchItems);
+console.log("DOM elements fetched:", searchInput, outputLabel, clearButton, exitButton, voiceButton);
 
-// Add voice recognition event listener
-voiceButton.addEventListener('click', () => {
+// Search Input Handling
+searchInput.addEventListener('input', function() {
+  const searchTerm = searchInput.value.toLowerCase();
+  let foundItems = pluData.filter(item =>  
+    item.Name.toLowerCase().includes(searchTerm) || 
+    item['PLU Code'].toString().includes(searchTerm)
+  );
+  
+  foundItems.sort((a, b) => a.Name.localeCompare(b.Name));
+  
+  outputLabel.innerHTML = '';
+  
+  for(let i = 0; i < Math.min(5, foundItems.length); i++) {
+    outputLabel.innerHTML += `<a href="https://www.google.com/search?q=${foundItems[i].Name}">${foundItems[i].Name}</a> (PLU Code: ${foundItems[i]['PLU Code']})<br>`;
+  }
+  
+  if (foundItems.length === 0) {
+    outputLabel.textContent = 'Not found';
+  }
+});
+
+// Voice Search Handling
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onstart = function() {
+  console.log('Voice recognition started. Speak into the microphone.');
+};
+
+recognition.onresult = function(event) {
+  const transcript = event.results[0][0].transcript;
+  console.log('You said: ', transcript);
+  searchInput.value = transcript;
+};
+
+recognition.onend = function() {
+  console.log('Voice recognition ended.');
+};
+
+voiceButton.addEventListener('click', function() {
   recognition.start();
 });
 
-recognition.addEventListener('result', function(event) {
-  searchInput.value = event.results[0][0].transcript;
-  searchItems();
+// Clear Button Handling
+clearButton.addEventListener('click', function() {
+  searchInput.value = '';
+  outputLabel.textContent = '';
 });
 
-function searchItems() {
-  const searchTerm = searchInput.value.toLowerCase();
-  let foundItems = pluData.filter(item =>
-    item.Name.toLowerCase().includes(searchTerm) ||
-    item['PLU Code'].toString().includes(searchTerm)
-  );
+// Exit Button Handling
+exitButton.addEventListener('click', function() {
+  window.close();
+});
 
-  foundItems.sort((a, b) => a.Name.localeCompare(b.Name));
-  outputLabel.innerHTML = '';
-
-  for (let i = 0; i < Math.min(5, foundItems.length); i++) {
-    outputLabel.innerHTML += `<a href="https://www.google.com/search?q=${foundItems[i].Name}">${foundItems[i].Name}</a> (PLU Code: ${foundItems[i]['PLU Code']})<br>`;
-  }
-}
-
-fetch('./plu_data.json')
-  .then(response => response.json())
+// Fetch PLU data
+fetch('./plu_data.json')  
+  .then(response => {
+    return response.json();
+  })
   .then(data => {
     pluData = data;
   })
