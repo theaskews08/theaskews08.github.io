b61b3f83c98c66d0eda9a8204aec49e6b15dbf1f
B <theaskews08@gmail.com>
2023-09-09 17:08:43 -0600

    Update main.js

--- main.js
+++ main.js
@@ -8,26 +8,29 @@ const clearButton = document.getElementById('clearButton');
 const exitButton = document.getElementById('exitButton');
 
 /* Event Listeners */
-searchInput.addEventListener('input', function() {
-  const searchTerm = searchInput.value.toLowerCase().split(' ');
-  let foundItems = pluData.filter(item => {
-    const itemNameTokens = item.Name.toLowerCase().split(' ');
+searchInput.addEventListener('keydown', function(event) {
+  if (event.keyCode === 13) {
+    // The Enter key was pressed
+    const searchTerm = searchInput.value.toLowerCase().split(' ');
+    let foundItems = pluData.filter(item => {
+      const itemNameTokens = item.Name.toLowerCase().split(' ');
 
-    return searchTerm.every(term => itemNameTokens.some(token => token.includes(term))) ||  
+      return searchTerm.every(term => itemNameTokens.some(token => token.includes(term))) ||  
            item['PLU Code'].toString().includes(searchTerm.join(' '));
-  });
+    });
 
-  // Clear the output label
-  outputLabel.innerHTML = '';
+    // Clear the output label
+    outputLabel.innerHTML = '';
 
-  // Sort and show the first 5 matched items as suggestions
-  foundItems.sort((a, b) => a.Name.localeCompare(b.Name));
-  for(let i = 0; i < Math.min(5, foundItems.length); i++) {
-    outputLabel.innerHTML += `<a href="https://www.google.com/search?q=${foundItems[i].Name}">${foundItems[i].Name}</a> (PLU Code: ${foundItems[i]['PLU Code']})<br>`;
-  }
+    // Sort and show the first 5 matched items as suggestions
+    foundItems.sort((a, b) => a.Name.localeCompare(b.Name));
+    for(let i = 0; i < Math.min(5, foundItems.length); i++) {
+      outputLabel.innerHTML += `<a href="https://www.google.com/search?q=${foundItems[i].Name}">${foundItems[i].Name}</a> (PLU Code: ${foundItems[i]['PLU Code']})<br>`;
+    }
 
-  if (foundItems.length === 0) {
-    outputLabel.textContent = 'Not found';
+    if (foundItems.length === 0) {
+      outputLabel.textContent = 'Not found';
+    }
   }
 });
 
