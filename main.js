cf4f66f058e329b145bd255fc0351a16067160db
B <theaskews08@gmail.com>
2023-09-09 07:17:04 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,73 +1,56 @@
-let pluData = [];
+// Initialize empty array for PLU data 
+let pluData = []; 
 
-const searchInput = document.getElementById('searchInput');
-const outputLabel = document.getElementById('outputLabel');
-const clearButton = document.getElementById('clearButton');
-const exitButton = document.getElementById('exitButton');
-const voiceButton = document.getElementById('voiceButton');
+// Get DOM elements 
+const searchInput = document.getElementById('searchInput'); 
+const outputLabel = document.getElementById('outputLabel'); 
+const clearButton = document.getElementById('clearButton'); 
+const exitButton = document.getElementById('exitButton'); 
 
-console.log("DOM elements fetched:", searchInput, outputLabel, clearButton, exitButton, voiceButton);
+// Event Listeners 
+searchInput.addEventListener('input', function() { 
+     const searchTerm = searchInput.value.toLowerCase(); 
+     let foundItems = pluData.filter(item =>  
+                                   item.Name.toLowerCase().includes(searchTerm) ||  
+                                   item['PLU Code'].toString().includes(searchTerm) 
+                                  ); 
 
-// Search Input Handling
-searchInput.addEventListener('input', function() {
-  const searchTerm = searchInput.value.toLowerCase();
-  let foundItems = pluData.filter(item =>  
-    item.Name.toLowerCase().includes(searchTerm) || 
-    item['PLU Code'].toString().includes(searchTerm)
-  );
-  
-  foundItems.sort((a, b) => a.Name.localeCompare(b.Name));
-  
-  outputLabel.innerHTML = '';
-  
-  for(let i = 0; i < Math.min(5, foundItems.length); i++) {
-    outputLabel.innerHTML += `<a href="https://www.google.com/search?q=${foundItems[i].Name}">${foundItems[i].Name}</a> (PLU Code: ${foundItems[i]['PLU Code']})<br>`;
-  }
-  
-  if (foundItems.length === 0) {
-    outputLabel.textContent = 'Not found';
-  }
-});
+     // Sort the items alphabetically 
+     foundItems.sort((a, b) => a.Name.localeCompare(b.Name)); 
 
-// Voice Search Handling
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
+     // Clear the output label 
+     outputLabel.innerHTML = ''; 
 
-recognition.onstart = function() {
-  console.log('Voice recognition started. Speak into the microphone.');
-};
+     // Show the first 5 matched items as suggestions 
+     for(let i = 0; i < Math.min(5, foundItems.length); i++) { 
+       outputLabel.innerHTML += `<a href="https://www.google.com/search?q=${foundItems[i].Name}">${foundItems[i].Name}</a> (PLU Code: ${foundItems[i]['PLU Code']})<br>`; 
+   } 
 
-recognition.onresult = function(event) {
-  const transcript = event.results[0][0].transcript;
-  console.log('You said: ', transcript);
-  searchInput.value = transcript;
-};
+     if (foundItems.length === 0) { 
+       outputLabel.textContent = 'Not found'; 
+   } 
+ }); 
 
-recognition.onend = function() {
-  console.log('Voice recognition ended.');
-};
+// Close keyboard on "Enter" key press 
+searchInput.addEventListener('keyup', function(event) { 
+     if (event.keyCode === 13) { 
+       searchInput.blur(); 
+   } 
+ }); 
 
-voiceButton.addEventListener('click', function() {
-  recognition.start();
-});
+clearButton.addEventListener('click', function() { 
+     searchInput.value = ''; 
+     outputLabel.textContent = ''; 
+ }); 
 
-// Clear Button Handling
-clearButton.addEventListener('click', function() {
-  searchInput.value = '';
-  outputLabel.textContent = '';
-});
+exitButton.addEventListener('click', function() { 
+     window.close(); 
+ }); 
 
-// Exit Button Handling
-exitButton.addEventListener('click', function() {
-  window.close();
-});
-
-// Fetch PLU data
-fetch('./plu_data.json')  
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    pluData = data;
-  })
-  .catch(error => console.error('Error fetching PLU data:', error));
+// Fetch data from plu_data.json and update pluData 
+fetch('./plu_data.json')   
+     .then(response => response.json()) 
+     .then(data => { 
+     pluData = data; 
+ }) 
+     .catch(error => console.error('Error fetching PLU data:', error));
