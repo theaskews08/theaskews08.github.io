645ee7c0b4c343538481628f6170d1d9c4a02a94
B <theaskews08@gmail.com>
2024-01-18 07:00:18 -0700

    Update Es.js

--- Es.js
+++ Es.js
@@ -1,1 +1,8 @@
-
+export default {
+  title: "Herramienta de BÃºsqueda PLU",
+  placeholder: "Buscar",
+  voiceSearch: "BÃºsqueda por Voz",
+  clear: "Limpiar",
+  exit: "Salir",
+  // Add other keys and translations here
+};
