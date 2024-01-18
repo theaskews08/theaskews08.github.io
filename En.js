7c5a0ecdc62b876f1c15a5577c355177cca3e8b4
B <theaskews08@gmail.com>
2024-01-18 07:01:49 -0700

    Update En.js

--- En.js
+++ En.js
@@ -1,1 +1,8 @@
-
+export default {
+  title: "PLU Lookup Tool",
+  placeholder: "Search",
+  voiceSearch: "Voice Search",
+  clear: "Clear",
+  exit: "Exit",
+  // Add other keys and translations here
+};
